d0b57b47f2dcced6fad423b201b1a5fda944af63
Kaho Otani <a1oe1ha@gmail.com>
2025-05-23 13:04:15 -0700

    added validation check for category/amount

--- script.js
+++ script.js
@@ -14,14 +14,37 @@ function getToday(){
 //information input section button click event
 document.querySelector('#informationInputSubmit').addEventListener('click', (e) => {
   e.preventDefault();
-  const date = document.querySelector('#today').value;
-  const amount = parseFloat((document.querySelector('#inputAmount').value)).toFixed(2); 
-    //parseFloatは小数点以下を含む数値に変換、toFixed(2)で小数点以下2桁にする
-  const descripttion = document.querySelector('#inputDescription') .value;
-  const category = document.querySelector('input[name="categories"]:checked').value;
+
+  //form > requiredの項目が入力されているかの確認
+  const form = document.querySelector('#expensesInput');
+  if(!form.checkVisibility()){
+    form.reportValidity();
+    return;
+  }
+
   const id = getLastId();
+  const date = document.querySelector('#today').value;
+  const descripttion = document.querySelector('#inputDescription') .value;
+
+  //categoryが選択されているか、されていなければalertで通知。選択後にそのvalueを取得
+  const selectCategory = document.querySelector('input[name="categories"]:checked');
+  if (!selectCategory){
+    alert("Please select a category.");
+    return;
+  }
+  const category = selectCategory.value;
+
+  //amountが入力されているか、されていなければalertで通知。選択後にそのvalueを取得
+  const amountValue = document.querySelector('#inputAmount').value;
+  if (!amountValue){
+    alert("Please add amount.");
+    return;
+  }
+  const amount = parseFloat((document.querySelector('#inputAmount').value)).toFixed(2); 
+      //parseFloatは小数点以下を含む数値に変換、toFixed(2)で小数点以下2桁にする
+
+  //各項目の入力後各データをdataに格納、localstrageに保存
   const data = {date: date, amount: amount, descripttion: descripttion, category: category};
-  
   localStorage.setItem(id, JSON.stringify(data));
 });
 
