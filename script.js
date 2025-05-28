6215f98bff86bdc65b46205b79a4b42f0fc0341b
Masamello <kojimasa1205@gmail.com>
2025-05-28 10:55:17 -0700

    Progress Bar Chart

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-function updateProgressBar(Expense, Badget) {
-    let percentage = (Expense / Badget) * 100;
-    document.querySelector(".progressBar").style.width = percentage + "%";
+// 
+const progress = localStorage.getItem('data') || 25;
+
+const context = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(context,{
+    type:'bar',
+    data:{
+        labels:progress,
+        datasets:[{
+            label:'Completion',
+            data:[progress],
+            backgroundColor:'rgba(75, 192, 192, 0.2)',
+            borderColor:'rgba(75, 192, 240, 1)',
+            borderWidth:1
+        }]
+    },
+    options:{
+        indexAxis: 'y',
+        scales: {
+            x: {
+                beginAtZero: true,
+                max: 100
+            }
+        }
+    }
+});
+
+function updateProgress(newProgress){
+    localStorage.setItem('progress',newProgress);
+    myChart.data.datasets[0].data = [newProgress];
+    myChart.update();
 }
-updateProgressBar(800, 1000);
+
